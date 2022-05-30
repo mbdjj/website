c89999059d38ee52db40c00e0116862e168377ef
Marcin Bartmiński <mbdjj@o2.pl>
2022-05-30 19:10:45 +0200

    :lipstick: Add rotation on hover over profile pic

--- src/components/main-page/ProfilePic.jsx
+++ src/components/main-page/ProfilePic.jsx
@@ -5,13 +5,14 @@ const ProfilePic = styled.img`
   border-radius: 50%;
   height: 128px;
   width: 128px;
-  transition: box-shadow 0.3s;
+  transition: box-shadow 0.3s, transform 0.3s;
 
   margin-top: 0;
   margin-bottom: 24px;
 
   &:hover {
     box-shadow: 0 0 10px 0 rgb(22, 25, 29);
+    transform: rotate(10deg);
   }
 
   @media (prefers-color-scheme: dark) {
