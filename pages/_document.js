7837fab8f8b2041f6d480bbf0bd4d16346bd3456
Marcin Bartmiński <mbdjj@o2.pl>
2023-01-22 21:45:32 +0100

    :sparkles: Add most of the main page

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;600"
+          rel="stylesheet"
+          type="text/css"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,5 @@
 import Head from 'next/head'
-//import Image from 'next/image'
-//import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
-import NavBar from '../components/navbar/NavBar'
-
-//const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -15,7 +10,52 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.png" />
       </Head>
-      
+      <div className={styles.websiteContainer}>
+        <div className={styles.contentContainer}>
+          <div className={styles.nav}>
+            <div className={styles.navContainer}>
+              <div className={styles.avatarLinkDiv}>
+                <a href='./' className={styles.avatarLink}>
+                  <div className={styles.avatarDiv}>
+                    <img src='images/memoji.png' className={styles.avatarImage} />
+                  </div>
+                </a>
+              </div>
+              <div className={styles.titleDiv}>
+                <h1>Marcin Bartmiński</h1>
+                <p className='caption'>iOS Developer</p>
+              </div>
+              <nav>
+                <div>
+                  <p>
+                    <a className='navLink selected' href='./'>About me</a>
+                  </p>
+                </div>
+                {/*<div>
+                  <p>
+                    <a className='navLink' href='./'>Projects</a>
+                  </p>
+                </div>
+                <div>
+                  <p>
+                    <a className='navLink' href='./'>Photos</a>
+                  </p>
+                </div>*/}
+              </nav>
+            </div>
+          </div>
+          <div className={styles.line}></div>
+          <div>
+            <p>
+              Hello! I'm Marcin, an iOS Developer and <a className='inlineLink' href='https://zsk.poznan.pl' target="_blank">ZSK</a> student.
+              In my free time I play video games, code, and watch movies.
+              You can follow me on <a className='inlineLink' href='https://twitter.com/bartmisnki' target="_blank">Twitter</a>, 
+              check out my projects on <a className='inlineLink' href='https://github.com/mbdjj' target="_blank">GitHub</a>, 
+              or send me an <a className='inlineLink' href='mailto:marcin@bartminski.ga' target="_blank">Email</a>.
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
