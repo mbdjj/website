e49fa647bab83a36dcc570336fec828a7f02072d
Marcin Bartmiński <mbdjj@o2.pl>
2023-02-02 16:55:46 +0100

    :sparkles: Add empty projects page

---
+++ pages/projects.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Projects() {
+  return (
+    <>
+      <Head>
+      <title>Projects</title>
+        <meta name="description" content="My personal portfolio website." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <div>
+        <h1>This page is under construction.</h1>
+      </div>
+    </>
+  );
+}
