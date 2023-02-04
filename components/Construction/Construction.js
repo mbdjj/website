f2ca50e95975cca188668be47e06d35e73307fa2
Marcin Bartmiński <mbdjj@o2.pl>
2023-02-04 12:11:13 +0100

    :sparkles: Add better construction page to projects

--- components/Construction/Construction.js
+++ components/Construction/Construction.js
@@ -1,9 +1,5 @@
-import styles from "./Construction.module.css";
-
 export default function Construction() {
   return (
-    <div className={styles.container}>
-      <h1>This page is under construction.</h1>
-    </div>
+    <h1>This page is under construction.</h1>
   );
 }
--- pages/projects.js
+++ pages/projects.js
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import SiteLayout from "../components/SiteLayout/SiteLayout";
+import Nav from "../components/Nav/Nav";
 import Construction from "../components/Construction/Construction";
 
 export default function Projects() {
@@ -10,7 +12,12 @@ export default function Projects() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.png" />
       </Head>
-      <Construction />
+      <SiteLayout>
+        <Nav selectedItem="projects" />
+        <div className="line"></div>
+
+        <Construction />
+      </SiteLayout>
     </>
   );
 }
