ec983637a3389d61d9905b582f38d08b1b1bf986
Marcin Bartmiński <mbdjj@o2.pl>
2023-02-05 10:13:05 +0100

    :construction: Start building projects UI

--- pages/projects.js
+++ pages/projects.js
@@ -2,12 +2,30 @@ import Head from "next/head";
 import SiteLayout from "../components/SiteLayout/SiteLayout";
 import Nav from "../components/Nav/Nav";
 import Construction from "../components/Construction/Construction";
+import styles from "../styles/Projects.module.css";
 
 export default function Projects() {
+  const projects = [
+    {
+      name: "Walut",
+      description: "",
+      category: "iOS App",
+      state: "Published",
+      link: "https://github.com/mbdjj/Walut",
+    },
+    {
+      name: "Walut",
+      description: "",
+      category: "iOS App",
+      state: "Published",
+      link: "https://github.com/mbdjj/Walut",
+    },
+  ];
+
   return (
     <>
       <Head>
-      <title>Projects</title>
+        <title>Projects</title>
         <meta name="description" content="My personal portfolio website." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.png" />
@@ -15,6 +33,16 @@ export default function Projects() {
       <SiteLayout>
         <Nav selectedItem="projects" />
         <div className="line"></div>
+
+        {projects.map((project) => {
+          return (
+            <div>
+              <h2>{projects[0].name}</h2>
+              <p>{projects[0].category}</p>
+              <p>{projects[0].state}</p>
+            </div>
+          );
+        })}
 
         <Construction />
       </SiteLayout>
