c8071d325099d4f9d8334c16c755dbc5690e5330
Marcin Bartmiński <mbdjj@o2.pl>
2024-03-10 13:48:12 +0100

    :sparkles: Add custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+export default function Custom404() {
+  return (
+    <>
+      <div className="errorContainer">
+        <h1 className="emoji">🤖🚫</h1>
+        <h2>404 - Page Not Found</h2>
+      </div>
+    </>
+  );
+}
