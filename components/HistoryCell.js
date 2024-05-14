2fe0d5b735888f67bb9c087dcdbb982747a9e4b6
Marcin Bartmiński <mbdjj@o2.pl>
2024-05-14 08:05:44 +0200

    :sparkles: Add title screen

---
+++ components/HistoryCell.js
@@ -0,0 +1,7 @@
+export default function HistoryCell() {
+  return (
+    <div className="bg-red-400 w-32 h-32 flex justify-center items-center rounded-full">
+      <p className="text-7xl">🔥</p>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
+import HistoryCell from "@/components/HistoryCell";
+
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-4xl">Hello, world!</h1>
+    <main className="flex flex-col">
+      <div className="flex flex-col w-screen h-screen justify-center items-center text-start bg-gradient-to-b from-blue-200 to-blue-50 dark:from-blue-900 dark:to-blue-950">
+        <div className="flex flex-col text-start">
+          <h1 className="text-5xl font-bold text-blue-950 dark:text-blue-50">
+            Marcin Bartmiński
+          </h1>
+          <h2 className="text-4xl text-blue-900 dark:text-blue-100">
+            An iOS Developer
+          </h2>
+        </div>
+      </div>
+      <div className="flex flex-col items-center p-8 bg-gradient-to-b from-blue-50 to-white dark:from-blue-950 dark:to-gray-900">
+        <HistoryCell />
+        <HistoryCell />
+        <HistoryCell />
+      </div>
     </main>
   );
 }
