62c4b7aa40f756165dea346c1d6e8203865c6da8
Marcin Bartmiński <mbdjj@o2.pl>
2024-05-15 20:36:20 +0200

    :recycle: Refactor titles and events to separate files

---
+++ data/events.js
@@ -0,0 +1,24 @@
+export const events = [
+  {
+    eventName: "Test event",
+    eventDescription: "A test event to check how the events will look.",
+    eventDate: Date.parse("2024-05-15"),
+    hasImage: false,
+    imageLink: "",
+    emoji: "🧪",
+    color: "bg-slate-400",
+    categories: ["Test"],
+    id: 0
+  },
+  {
+    eventName: "Second test event",
+    eventDescription: "A test event to check how the events will look. Now with even longer description to check how the scaling works. :)",
+    eventDate: Date.parse("2024-05-15"),
+    hasImage: false,
+    imageLink: "",
+    emoji: "👨‍🔬",
+    color: "bg-slate-500",
+    categories: ["Test"],
+    id: 1
+  }
+]
---
+++ data/titles.js
@@ -0,0 +1,6 @@
+export const titles = [
+  "An iOS Developer",
+  "IT High School Graduate",
+  "Epic Gamer",
+  "Walut's creator",
+];
