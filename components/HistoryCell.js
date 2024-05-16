6528806f0208f25a9b30b8a51eeac38f1de2c7d7
Marcin Bartmiński <mbdjj@o2.pl>
2024-05-16 12:14:44 +0200

    :iphone: Fix open event responsiveness

--- components/HistoryCell.js
+++ components/HistoryCell.js
@@ -4,7 +4,7 @@ export default function HistoryCell({ event, isLast }) {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="flex flex-col items-center relative mt-[-0.125rem] transition-all">
+    <div className="flex flex-col items-center relative mt-[-2px] transition-all w-[100%]">
       <div className={`${event.color} w-32 h-32 flex justify-center items-center rounded-full z-20`} onClick={
         () => {
           setIsOpen((is) => !is)
@@ -12,11 +12,11 @@ export default function HistoryCell({ event, isLast }) {
       }>
         <p className="text-7xl">{event.emoji}</p>
       </div>
-      <div className={`${isOpen ? "event-open" : "event-closed"} mt-[-8rem] w-32 h-32 rounded-full flex flex-col bg-blue-100 py-6 px-10 items-center z-10`}>
+      <div className={`${isOpen ? "event-open" : "event-closed"} mt-[-7.5rem] w-28 h-28 max-w-[90%] rounded-full flex flex-col bg-blue-100 py-6 px-10 items-center z-10`}>
         <h2 className="text-2xl font-bold opacity-0">{event.eventName}</h2>
         <p className="text-neutral-500 opacity-0 overflow-hidden">{event.eventDescription}</p>
       </div>
-      {isLast ? <></> : <div className="bg-gradient-to-b from-slate-400 to-slate-500 w-6 h-24 mt-[-0.125rem]" />}
+      {isLast ? <></> : <div className="bg-gradient-to-b from-slate-400 to-slate-500 w-6 h-24" />}
     </div>
   );
 }
